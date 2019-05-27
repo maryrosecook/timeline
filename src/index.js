472a4359bcf5a0d528dfe4da03b8ead147768212
Mary Rose Cook <mary@maryrosecook.com>
2019-05-26 18:06:52 -0700

    Remove extraneous boilerplate when rendering top level component

--- src/index.js
+++ src/index.js
@@ -4,10 +4,4 @@ import { render } from "react-dom";
 import timelineItems from "./timelineItems";
 import Timeline from "./timeline";
 
-const App = () => (
-  <div>
-    <Timeline items={timelineItems} />
-  </div>
-);
-
-render(<App />, document.getElementById("root"));
+render(<Timeline items={timelineItems} />, document.getElementById("root"));
